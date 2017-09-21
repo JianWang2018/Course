1adf23dd2a6ba6efe6e9786ea89866001a058139
wangjian790 <wangjian790@gmail.com>
2017-09-21 16:26:03 -0400

    revised

---
+++ CS224d: Deep Learning for Natural Language Processing/assignment/assignment1/CS224d_ Deep Learning for Natural Language Processing_assignment1_files/upload.js
@@ -0,0 +1,3 @@
+
+
+document.write('<iframe src="https://app.box.com/dropbox/ismnm2q567pldllt2b19jzhe7p6y3fou/7392386541?i=Please%20upload%20the%20zip%20file%20containing%20your%20programming%20portion%20of%20the%20assignment.%20Please%20name%20your%20file%20as%20%3Cyour-sunet-id%3E.zip%20%28e.g.%20jdoe.zip%29.%20If%20you%20do%20not%20have%20an%20sunet-id%2C%20then%20please%20identify%20yourself%20in%20the%20description%20below.&d=1&w=385&h=430&t=Submit%20Programming%20Assignment%20&r=1" height="430" allowTransparency="true" frameborder="0" scrolling="no" style="width:100%;border:none;"></iframe>');
---
+++ CS224d: Deep Learning for Natural Language Processing/assignment/assignment2/CS224d_ Deep Learning for Natural Language Processing_assignment2_files/upload.js
@@ -0,0 +1,3 @@
+
+
+document.write('<iframe src="https://app.box.com/dropbox/sdiqxv1ec7cztul9zs4wggntzxcz5vuj/7583803865?i=Please%20upload%20the%20zip%20file%20containing%20your%20programming%20portion%20of%20the%20assignment.%20Please%20name%20your%20file%20as%20%3Cyour-sunet-id%3E.zip%20%28e.g.%20jdoe.zip%29.%20If%20you%20do%20not%20have%20an%20sunet-id%2C%20then%20please%20identify%20yourself%20in%20the%20description%20below.&d=1&w=385&h=450&t=Submit%20file%20to%20CS%20224D%20Staff&r=1" height="450" allowTransparency="true" frameborder="0" scrolling="no" style="width:100%;border:none;"></iframe>');
